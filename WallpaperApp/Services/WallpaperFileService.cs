c9d5d0222486550772eda2386079b3ae53dda468
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-30 12:34:02 +0200

    [~] Add ApplicationException in GetWallpapers()

--- WallpaperApp/Services/WallpaperFileService.cs
+++ WallpaperApp/Services/WallpaperFileService.cs
@@ -8,17 +8,31 @@ namespace WallpaperApp.Services
         string WallpaperFolderPath = "D:\\WallpaperAppPicture";
         public List<Wallpaper> GetWallpapers()
         {
-            List<string> files = Directory.GetFiles(WallpaperFolderPath, "*.jpg").ToList();
+            List<string> files;
+			List<Wallpaper> wallpapers = new List<Wallpaper>();
 
-            List<Wallpaper> wallpapers = new List<Wallpaper>();
-
-            foreach (var file in files)
+			try
             {
-                wallpapers.Add(new Wallpaper());
+				files = Directory.GetFiles(WallpaperFolderPath, "*.jpg").ToList();
+
+				foreach (var file in files)
+				{
+					wallpapers.Add(new Wallpaper(Path.GetFileName(file), file));
+				}
+			}
+            catch(Exception ex)
+            {
+				throw new ApplicationException("An exception has been thrown during retrieval of wallpapers.", ex);
 
 			}
 
-            return null;
+
+			if (!wallpapers.Any())
+            {
+                throw new ApplicationException("No files has been found in the provided directory.");
+            }
+
+            return wallpapers;
         }
     }
 }
